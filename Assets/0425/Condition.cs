5d0d0e32b02ac8a2fd32c815e06d3b7f75bda0c6
Ryusei1115 <tekunikaru22@gmail.com>
2023-04-25 12:25:41 +0900

    wip

--- Assets/0425/Condition.cs
+++ Assets/0425/Condition.cs
@@ -11,6 +11,9 @@ public class Condition : MonoBehaviour
     private byte _poison = 0b0000_0001;
 
     [SerializeField]
+    private byte _deadlyPoison = 0b0001_0000;
+
+    [SerializeField]
     private byte _paralysis = 0b0000_0010;
 
     [SerializeField]
@@ -30,32 +33,43 @@ public class Condition : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.R))
         {
-            _player |= _poison;
+            _player = _player < _poison ? _poison : _deadlyPoison;
+
+            //_player |= _poison;
+
             Debug.Log("Poison => Condition" + System.Convert.ToString(_player, 2).PadLeft(4, '0'));
         }
 
         if(Input.GetKeyDown(KeyCode.F))
         {
-            _player |= _paralysis;
+            _player |= _paralysis;// |= or演算
             Debug.Log("Paralysis => Condition" + System.Convert.ToString(_player, 2).PadLeft(4, '0'));
+            // 2進数にするためToStringの第2引数に2を入れている
+            // PadLeftの第1引数は4桁であることを表し、第2引数は0埋め用に入れている
         }
 
-        if(Input.GetKeyDown(KeyCode.V))
+        if (Input.GetKeyDown(KeyCode.V))
         {
             _player |= _sleep;
             Debug.Log("Sleep => Condition" + System.Convert.ToString(_player, 2).PadLeft(4, '0'));
         }
 
-        if(Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.T))
         {
             _player |= _silence;
             Debug.Log("Silence => Condition" + System.Convert.ToString(_player, 2).PadLeft(4, '0'));
         }
 
-        if(Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             _player = _initialData;
             Debug.Log("InitialData => Condition" + System.Convert.ToString(_player, 2).PadLeft(4, '0'));
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            _player &= (byte) ~ _poison;
+            Debug.Log("PoisonCare => Condition" + System.Convert.ToString(_player, 2).PadLeft(4, '0'));
         }
     }
 }
